a104003e3e1c73d171dd334880c8c20b00dfec25
Bemnet-21 <anjejobemnet@gmail.com>
2026-03-30 22:01:34 +0300

    fix(auth): fix handling of expired token

--- jobsphere/client/src/App.tsx
+++ jobsphere/client/src/App.tsx
@@ -1,19 +1,50 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import JobDetails from "./pages/JobDetails";
 import Header from "./components/layout/Header";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "./store/store";
 import AddJob from "./pages/AddJob";
 import Signup from "./pages/Signup";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import type { User } from "./interface";
+import { logout } from "./store/authSlice";
 
 function App() {
   const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token && !isAuthenticated) {
+      navigate('/');
+    } else if (!token && isAuthenticated) {
+      navigate('/login');
+    }
+
+    try {
+      if (token) {
+        const decoded = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp && decoded.exp < currentTime) {
+          dispatch(logout())
+          navigate('/login')
+        }
+
+      }
+    } catch (error) {
+        dispatch(logout())
+        navigate('/login')
+    }
+
+  }, [navigate])
 
   return (
-    <BrowserRouter>
+    <>
       {isAuthenticated && <Header />}
 
       <Routes>
@@ -34,7 +65,7 @@ function App() {
 
         <Route path="*" element={<Navigate to={isAuthenticated ? "/" : "/login"} />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
